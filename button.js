d42b478921ca3f9a3cb3e6ae403a32356d051718
78redok <81646320+78redok@users.noreply.github.com>
2021-04-14 10:05:08 -0700

    added resume feature for button

--- button.js
+++ button.js
@@ -1,3 +1,6 @@
-function button(){
-    alert("Hello World!");
+function download(){
+    if (confirm ('This Resume is a Work in Progress')) {
+        location.href = './resume.pdf';
+    }
+    return false;
 }
